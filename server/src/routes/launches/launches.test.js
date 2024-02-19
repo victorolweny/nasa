09377281e7b307557b52e2b1641c9007bf3eb58b
Victor O <victorolweny@gmail.com>
2024-02-19 14:13:49 +0300

    Load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest')
 const app = require('../../app')
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo')
+const { loadPlanetsData } = require('../../models/planets.models')
 
 describe('Lauches API', () => {
   beforeAll(async () => {
     await mongoConnect()
+    await loadPlanetsData()
   })
 
   afterAll(async () => {
